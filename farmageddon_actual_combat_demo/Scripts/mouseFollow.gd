1900091f52c5e6bf65b2f4b76ced601b32082af7
LucianoPerez314 <lnp3@rice.edu>
2025-01-13 14:45:33 -0600

    Revert "Initial Commit"
    This reverts commit 8096141a173a56f2805e6af6b7593034ffc4653a.

--- farmageddon_actual_combat_demo/Scripts/mouseFollow.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(_delta: float) -> void:
-	var mouse_position := get_global_mouse_position()
-	position = mouse_position
--- farmageddon_actual_combat_demo/Scripts/shooter.gd
+++
@@ -1,6 +0,0 @@
-extends CharacterBody2D
-
-signal shoot
-
-func _physics_process(_delta: float) -> void:
-	emit_signal("shoot", "hold")

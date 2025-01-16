c6a30051e71f52db317eaacba5f56ad9a267dcca
LucianoPerez314 <lnp3@rice.edu>
2025-01-15 21:39:46 -0600

    Tweaked shooter

--- Scenes/shootState.gd
+++ Scenes/shootState.gd
@@ -1,14 +1,19 @@
 extends State
 
-
+@export var time_shooting: int
 var enemy: CharacterBody2D
+var time: int
 
 func Enter():
+	time = 0
 	enemy = get_parent().get_parent()
 	
 func Update(delta: float):
+	time += 1
 	enemy.emit_signal("shoot", "hold")
 	enemy.emit_signal("shoot", "tap")
+	if time >= time_shooting:
+		emit_signal("state_transition", self, "Follow")
 
 func Exit():
-	pass
+	time = 0

9dc492c61f61b7c958f69878056ecf19069d53f5
Andrew Holzbach <andrewholzbach@andrews-mbp-4.dyndns.rice.edu>
2025-01-17 12:34:37 -0600

    added player animations

--- Scripts/idleState.gd
+++ Scripts/idleState.gd
@@ -2,9 +2,11 @@ extends State
 class_name IdleState
 
 var character: CharacterBody2D
+var mc: Node
 
 func Enter():
 	character = get_parent().get_parent()
+	mc = get_parent()
 	
 func Update(_delta: float):
 	if Input.get_vector("move_left", "move_right", "move_up", "move_down"):
@@ -12,6 +14,7 @@ func Update(_delta: float):
 	elif Input.is_action_just_pressed("dash"):
 		emit_signal("state_transition", self, "Dash")
 	else:
+		mc.animation.play("idle")
 		#character.velocity = Vector2.ZERO
 		character.move_and_slide()
 func Exit():

b960fd8e7b304b5d5a8c152167831e07474248dd
LucianoPerez314 <lnp3@rice.edu>
2025-01-20 19:08:01 -0600

    FEAT: Added procedurally animated legs

--- Scripts/snake.gd
+++ Scripts/snake.gd
@@ -1,32 +1,36 @@
 extends Node2D
 
+
 # TODO: Add way to manually tweak size of each point.
 @export var num_segments: int = 1
 @export_range(0.0,1.0)
 var stiffness: float
 
 var segments: Array[Node2D] = []
+var is_ready: bool
 
 @export var distance: float
 @export var view_lines: bool
-@export var line_width: float = 10
+@export var line_width: float = 1
 
-var lines: Array[Line2D] = []
-
-@export var legged_segments: Array[int]
-@export var leg_distance: float
-@export var leg_spread: float
-
+var line: Line2D
 
 func _ready() -> void:
 	segments_factory()
-	legs_factory()
+	if view_lines:
+		line = Line2D.new()
+		line.width = line_width
+		get_tree().current_scene.add_child.call_deferred(line)
+	is_ready = true
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta: float) -> void:
 	make_head_follow_mouse()
 	make_segments_follow_each_other()
+	
+	if view_lines:
+		draw_lines()
 
 func segment_factory() -> Node2D:
 	var node: Node2D = Node2D.new()
@@ -40,22 +44,7 @@ func segments_factory() -> void:
 		var segment = segment_factory()
 		get_tree().current_scene.add_child.call_deferred(segment)
 		segments.append(segment)
-		if view_lines and i != 0:
-			var line = Line2D.new()
-			line.width = line_width
-			get_tree().current_scene.add_child.call_deferred(line)
-			lines.append(line)
 
-func legs_factory() -> void:
-	for i in legged_segments:
-		var leg1 = segment_factory()
-		var leg2 = segment_factory()
-		segments[i].add_child(leg1)
-		segments[i].add_child(leg2)
-		leg1.position = Vector2(cos(deg_to_rad(leg_spread)), sin(deg_to_rad(leg_spread))).normalized() * leg_distance
-		leg2.position =  Vector2(cos(deg_to_rad(-1 * leg_spread)), sin(deg_to_rad(-1 * leg_spread))).normalized() * leg_distance
-		
-	
 # TODO: Copied this from mouefollower, change this or mousefollower later.
 func make_head_follow_mouse() -> void:
 	var mouse_position := get_global_mouse_position()
@@ -77,7 +66,9 @@ func make_segments_follow_each_other() -> void:
 		# Smoothly move towards the desired position
 		current_node.position = current_node.position.lerp(desired_position, stiffness)
 		current_node.look_at(prev_node.position)
-		if view_lines:
-			lines[i - 1].points = [prev_node.position, current_node.position]
 		
-		
+func draw_lines() -> void:
+	line.clear_points()
+	for i in range(segments.size()):
+		var current_node = segments[i]
+		line.add_point(current_node.position)

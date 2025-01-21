8ef968c377079398cec81fe504cf4cddbe6e56ba
Seyaul <ericskim0510@gmail.com>
2025-01-21 16:14:28 -0600

    test commit to try merging branches w nodes

--- Scripts/CharacterHealth.gd
+++ Scripts/CharacterHealth.gd
@@ -2,6 +2,8 @@ extends Node
 
 @export var max_health: float = 100
 var current_health: float
+signal damage_taken
+signal healed
 
 var character: CharacterBody2D
 func _ready() -> void:
@@ -14,6 +16,7 @@ func _physics_process(delta: float) -> void:
 	
 func take_damage(amount: float) -> void:
 	current_health -= amount
+	damage_taken.emit()
 	if current_health <= 0:
 		current_health = 0
 		die()
@@ -21,6 +24,7 @@ func take_damage(amount: float) -> void:
 		print(character.name, " health:", current_health)
 
 func heal(amount: float) -> void:
+	healed.emit()
 	current_health += amount
 	if current_health > max_health:
 		current_health = max_health

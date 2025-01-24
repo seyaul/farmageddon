3924e55c185daa60edda2858fd350fbdc1cc94eb
Seyaul <ericskim0510@gmail.com>
2025-01-24 15:38:26 -0600

    removing the tractor dingaling

--- Scripts/idleState.gd
+++ Scripts/idleState.gd
@@ -6,7 +6,7 @@ var mc: Node
 
 func Enter():
 	character = get_parent().get_parent()
-	mc = get_parent()
+	#mc = get_parent()
 	
 func Update(_delta: float):
 	if Input.get_vector("move_left", "move_right", "move_up", "move_down"):
@@ -14,7 +14,7 @@ func Update(_delta: float):
 	elif Input.is_action_just_pressed("dash"):
 		emit_signal("state_transition", self, "Dash")
 	else:
-		mc.animation.play("idle")
+		#mc.animation.play("idle")
 		#character.velocity = Vector2.ZERO
 		character.move_and_slide()
 func Exit():

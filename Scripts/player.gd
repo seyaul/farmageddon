c0f9bc27ec27f054700bdc8e2ffbc9cf70546652
Seyaul <ericskim0510@gmail.com>
2025-01-24 15:30:39 -0600

    added player animation

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -7,6 +7,7 @@ signal shockwave
 var ammo: int = 100
 var max_ammo: int = 100
 var ammo_bar: Label
+var animation: AnimatedSprite2D
 
 func _ready() -> void:
 	var crosshairs = get_node("../Crosshairs")
@@ -14,6 +15,7 @@ func _ready() -> void:
 	ammo_bar = get_node("../UserInterfaceLayer/PlayerUI/Ammo")
 	var gun = $pistol
 	gun.bullet_fired.connect(_on_bullet_fired)
+	animation = get_node("AnimatedSprite2D")
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("shoot"):
@@ -22,12 +24,19 @@ func _physics_process(delta: float) -> void:
 		emit_signal("shoot", "hold", delta)
 	elif Input.is_action_just_released("shoot"):
 		emit_signal("shoot", "end", delta)
-
 	if Input.is_action_just_pressed("melee"):
 		emit_signal("melee")
-	
 	if Input.is_action_just_pressed("shockwave"):
 		emit_signal("shockwave")
+	if Input.is_action_just_pressed("move_down"):
+		animation.play("frontFacingWalk")
+	elif Input.is_action_just_pressed("move_up"):
+		animation.play("backFacingWalk")
+	elif Input.is_action_just_pressed("move_left"):
+		animation.play("leftFacingWalk")
+	elif Input.is_action_just_pressed("move_right"):
+		animation.play("rightFacingWalk")
+	
 
 func reload():
 	ammo = max_ammo

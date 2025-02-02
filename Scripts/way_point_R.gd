009dac25f1e674d5a9a967a434e0149f022dc3b0
Seyaul <ericskim0510@gmail.com>
2025-02-02 13:35:49 -0600

    waypoint experimentation, no more growing

--- Scripts/way_point_R.gd
+++ Scripts/way_point_R.gd
@@ -15,8 +15,9 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if player: 
-		time += delta * oscillationSpeed
-		var newPos = Vector2(base_pos.x + abs(sin(time)) * oscillationMagnitude, 0)
-		self.position = newPos
+	#if player: 
+		#time += delta * oscillationSpeed
+		#var newPos = Vector2(base_pos.x + abs(sin(time)) * oscillationMagnitude, 0)
+		#self.position = newPos
+	pass
 		

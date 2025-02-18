be61aa75402f9f3ee014b850b906bd2e4188af37
Chloe Lim <chloelim608@gmail.com>
2025-02-18 08:54:46 -0600

    wall gen still not working rip

--- Scenes/tilesets/walls_generator.gd
+++ Scenes/tilesets/walls_generator.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 @export var walls_tilemap_layer: TileMapLayer
-@export var wall_tile_id: int = 0
+@export var wall_tile_id: int= 2
 @export var grid_width: int = 70
 @export var grid_height: int = 39
 
@@ -12,14 +12,16 @@ func _ready():
 func generate_random_walls():
 	for i in range(4):  # number of walls to generate
 		# random dimensions for the wall (2x2 to 20x2 or 2x2 to 2x20)
-		var wall_width = randf_range(2, 20) 
-		var wall_height = randf_range(2, 20) 
+		var wall_width = int(randf_range(2, 20))
+		var wall_height = int(randf_range(2, 20))
 
 		# random starting position for wall
-		var start_x = randf_range(0, grid_width - wall_width)
-		var start_y = randf_range(0, grid_height - wall_height)
+		var start_x = int(randf_range(0, grid_width - wall_width))
+		var start_y = int(randf_range(0, grid_height - wall_height))
+		print("Wall generated at: ", start_x, start_y)
 
 		# place wall tiles on the grid in Walls
 		for x in range(wall_width):
 			for y in range(wall_height):
 				walls_tilemap_layer.set_cell(Vector2(start_x + x, start_y + y), wall_tile_id)
+				print("Placed tile at: ", start_x + x, start_y + y)

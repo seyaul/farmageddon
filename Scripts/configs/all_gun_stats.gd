ba32639e4b2778c8f43de7b325dc74a3a9df7a2e
Andrew Holzbach <andrewholzbach@Andrews-MacBook-Pro-5.local>
2025-02-25 23:23:24 -0600

    small

--- Scripts/configs/all_gun_stats.gd
+++ Scripts/configs/all_gun_stats.gd
@@ -7,3 +7,8 @@ class_name AllGunStats
 # this will be multiplied by the base cooldown speed of every gun, ex: if 2 will make 
 # cooldown circle retreat in half the time
 @export var cooldown_speed_modifier: float = 1
+
+
+func reset_to_defaults():
+    fire_rate_modifier = 1
+    cooldown_speed_modifier = 1

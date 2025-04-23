224f866c563613485754c566983dd7cf4ae9c1bb
Andrew Holzbach <andrewholzbach@Andrews-MacBook-Pro-272.local>
2025-04-23 15:59:05 -0500

    added credits

---
+++ Scripts/credits.gd
@@ -0,0 +1,17 @@
+extends Control
+
+# Set the speed of the credits scroll (pixels per second)
+var scroll_speed: float = 0.2
+var scroll_accumulation: float = 0
+
+# Called when the node enters the scene tree
+func _ready():
+    # Optional: start at the top
+    $ScrollContainer.scroll_vertical = 0
+
+func _process(delta):
+    scroll_accumulation += scroll_speed
+    if scroll_accumulation >= 1:
+        scroll_accumulation = 0
+        var v_scroll = $ScrollContainer.scroll_vertical
+        $ScrollContainer.scroll_vertical = v_scroll + 1
